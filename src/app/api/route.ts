10dc6022ee1da35535e5f8a2fac58ef2e9890fb5
Jonathan <jonathanrobaustin@gmail.com>
2025-02-16 21:43:36 -0600

    finally got first api working

---
+++ src/app/api/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+// In-memory storage for generated IDs
+const generatedIds = new Set<string>();
+
+function generateId() {
+    let id;
+    do {
+      id = Math.random().toString(36).replace(/[^a-zA-Z]/g, '').substring(0, 6).toUpperCase();
+    } while (generatedIds.has(id) || id.length !== 6);
+    generatedIds.add(id);
+    return id;
+  }
+
+export async function POST() {
+  const id = generateId();
+  console.log(id);
+  return NextResponse.json({ id });
+}
+
+export async function GET() {
+  const id = generateId();
+  console.log(id);
+  console.log(generatedIds)
+  
+  return NextResponse.json({ id });
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,15 @@
 'use client'
 import { Pacifico, Quicksand } from "next/font/google";
-import Button from "./Button";
+import Button from "../Button";
 import { get } from "http";
-import { useState } from "react";
 
-const handleClick = async (setResponse: React.Dispatch<React.SetStateAction<string>>) => {
-  const response = await fetch ("/api",{
-    method: "GET"
-  });
-  console.log(response);
-  const data = await response.json();
-  setResponse(data.id);
-
+const handleClick = async () => {
+  await fetch ("/api/GenerateRoomCode",{});
 }
 
 const pacif = Pacifico({ weight: "400", subsets: ['latin'] });
-const qs = Quicksand({ weight: "400", style: "normal", subsets: ['latin'] });
+const qs = Quicksand({ weight: "400", style: "normal", subsets: ['latin'] })
 export default function Home() {
-  const [response, setResponse] = useState<string>('');
   return (
     <div className={`flex flex-col grow` }>
       <div className={`bg-red-300 border-8 border-white rounded-2xl flex flex-col w-1/2 h-1/2 ${pacif.className} justify-center m-auto p-10`}>
@@ -25,9 +17,8 @@ export default function Home() {
           <p>Welcome to Meat Metronome. The tool to help you keep the <span className="text-red-600 pun">beats</span> of your "<span className="text-red-600 pun">meating</span>".</p>
         </div>
         <div className="flex flex-row py-4 items-center justify-center justify-evenly text-3xl">
-          <Button text="Sign Up" onClick={() => handleClick(setResponse)}></Button>
-          <Button text="Log In" onClick={() => handleClick(setResponse)}></Button>
-          <p>{response}</p>
+          <Button text="Sign Up" onClick={handleClick}></Button>
+          <Button text="Log In" onClick={handleClick}></Button>
         </div>
       </div>
     </div>
