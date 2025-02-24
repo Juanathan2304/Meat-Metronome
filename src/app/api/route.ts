015b67e82159c6d07033579cfac5927638c7ff05
Jonathan <jonathanrobaustin@gmail.com>
2025-02-23 18:43:48 -0600

    resync

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,37 +3,35 @@ import { NextResponse } from "next/server";
 // In-memory storage for generated IDs
 const generatedIds = new Set<string>();
 
-
-
 function generateId() {
     let id;
     do {
-      id = Math.random().toString(36).replace(/[^a-zA-Z]/g, '').substring(0, 6).toUpperCase();
+        id = Math.random().toString(36).replace(/[^a-zA-Z]/g, '').substring(0, 6).toUpperCase();
     } while (generatedIds.has(id) || id.length !== 6);
     generatedIds.add(id);
     return id;
-  }
-
-export async function POST() {
-  const id = generateId();
-  console.log(id);
-  return NextResponse.json({ id });
 }
 
-export async function GET() {
-  const id = generateId();
-  console.log(id);
-  console.log(generatedIds)
-  
-  return NextResponse.json({ id });
+// Define an interface for the Room JSON response
+interface AgendaItem {
+    title: string;
+    time: number;
 }
 
 interface Room {
-  room: string;
-  agenda: [
-    {
-      title: string;
-      time: number;
-    }
-  ];
+    room: string;
+    agenda: AgendaItem[];
+}
+
+export async function POST() {
+    const id = generateId();
+    console.log(id);
+    return NextResponse.json({ id });
+}
+
+export async function GET() {
+    const id = generateId();
+    console.log(id);
+    console.log(generatedIds);
+    return NextResponse.json({ id });
 }
